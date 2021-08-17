2b9d4d473f649ec42e4ddbb82a173cb1fa24d5e2
Ty Arnold <tyarnold1@gmail.com>
2021-08-17 18:48:38 -0500

    Push Updated
    Publish

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from "./App"
+// import AppForm from "./AppForm"
+// import FormControl from "./components/FormControl"
+import FormControl2 from "./components/FormControl2"
+
+ReactDOM.render(
+  <FormControl2 />,
+  // <FormControl2 />,
+  document.getElementById('root')
+);
+
